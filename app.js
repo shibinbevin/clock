48ddc8f51bf04298d1b15d134c3032a5c2fbf3eb
Shibin-b <shibinbevin123@gmail.com>
2023-09-12 00:02:11 +0530

    added date

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function showTime(){
     var time = new Date().toLocaleTimeString('en-US');
+    var date = new Date().toLocaleDateString('en-US');
 
     document.querySelector('#time').innerHTML = time;
+    document.querySelector('#date').innerHTML = date;
 
     setTimeout(showTime, 1000);
 }
