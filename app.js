198e98f33fdfecc1eacd7f7afe9b517f2393d6ee
Shibin-b <shibinbevin123@gmail.com>
2023-09-11 23:03:39 +0530

    changed to toLocaleTimeString() method to get the time

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 function showTime(){
-    var date = new Date();
+    var time = new Date().toLocaleTimeString('en-US');
 
-    var hours = date.getHours();
-    if( hours < 10 )hours = "0" + hours;
-    var minutes = date.getMinutes();
-    if( minutes < 10 )minutes = "0" + minutes;
-    var seconds = date.getSeconds();
-    if( seconds < 10 )seconds = "0" + seconds;
+    document.querySelector('#time').innerHTML = time;
 
-    document.querySelector('.time').innerHTML = hours + ':' + minutes + ':' + seconds;
     setTimeout(showTime, 1000);
 }
